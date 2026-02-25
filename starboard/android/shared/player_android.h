xiaomings/cobalt

commit 3134738f0fddc6b2ce8c616e1939e2450ecca71f

media: Reduce boilerplate plumbing between SbPlayerBridge and VideoDecoder (#9183)
Consolidate various video decoder experimental parameters into a single
struct. This change updates the SbPlayerBridge, PlayerWorker::Handler,
and StarboardExtensionVideoDecoderConfigurationApi to pass these flags
as a structured object.

This refactoring simplifies the process of adding new experimental flags
by removing the need to modify multiple API signatures. It improves
maintainability and extensibility of the video decoder configuration.

For plumbing from SbPlayerBridge to VideoDecoder

Before
- Necessary change: 18 files changed, 87 insertions(+), 9 deletions(-)
-
[example](https://github.com/youtube/cobalt/pull/9182/changes/dccf247cdcec4ddbb2a4f3053b1e20b559730fdb)

After
- Necessary change: 8 files changed, 18 insertions(+), 1 deletion(-)
-
[example](https://github.com/youtube/cobalt/pull/9144/changes/9af05324891956a4ce20a26ad5130265ded419eb)

Bug: 480134029

starboard/android/shared/player_android.h
@@ -1,0 +1,84 @@
+// Copyright 2026 The Cobalt Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef STARBOARD_ANDROID_SHARED_PLAYER_ANDROID_H_
+#define STARBOARD_ANDROID_SHARED_PLAYER_ANDROID_H_
+
+#include <memory>
+#include <utility>
+
+#include "starboard/android/shared/video_decoder_experimental_features.h"
+#include "starboard/shared/starboard/player/player_internal.h"
+#include "starboard/shared/starboard/player/player_worker.h"
+
+namespace starboard::android::shared {
+
+class SbPlayerAndroid
+    : public ::starboard::shared::starboard::player::SbPlayerPrivateImpl {
+ public:
+  static SbPlayerAndroid* Create(
+      SbMediaAudioCodec audio_codec,
+      SbMediaVideoCodec video_codec,
+      SbPlayerDeallocateSampleFunc sample_deallocate_func,
+      SbPlayerDecoderStatusFunc decoder_status_func,
+      SbPlayerStatusFunc player_status_func,
+      SbPlayerErrorFunc player_error_func,
+      void* context,
+      std::unique_ptr<
+          ::starboard::shared::starboard::player::PlayerWorker::Handler>
+          player_worker_handler,
+      const VideoDecoderExperimentalFeatures& experimental_features) {
+    auto player = std::unique_ptr<SbPlayerAndroid>(new SbPlayerAndroid(
+        audio_codec, video_codec, sample_deallocate_func, decoder_status_func,
+        player_status_func, player_error_func, context,
+        std::move(player_worker_handler), experimental_features));
+    if (!player->worker_) {
+      return nullptr;
+    }
+    return player.release();
+  }
+
+  const VideoDecoderExperimentalFeatures& experimental_features() const {
+    return experimental_features_;
+  }
+
+ private:
+  SbPlayerAndroid(
+      SbMediaAudioCodec audio_codec,
+      SbMediaVideoCodec video_codec,
+      SbPlayerDeallocateSampleFunc sample_deallocate_func,
+      SbPlayerDecoderStatusFunc decoder_status_func,
+      SbPlayerStatusFunc player_status_func,
+      SbPlayerErrorFunc player_error_func,
+      void* context,
+      std::unique_ptr<
+          ::starboard::shared::starboard::player::PlayerWorker::Handler>
+          player_worker_handler,
+      const VideoDecoderExperimentalFeatures& experimental_features)
+      : SbPlayerPrivateImpl(audio_codec,
+                            video_codec,
+                            sample_deallocate_func,
+                            decoder_status_func,
+                            player_status_func,
+                            player_error_func,
+                            context,
+                            std::move(player_worker_handler)),
+        experimental_features_(experimental_features) {}
+
+  const VideoDecoderExperimentalFeatures experimental_features_;
+};
+
+}  // namespace starboard::android::shared
+
+#endif  // STARBOARD_ANDROID_SHARED_PLAYER_ANDROID_H_

starboard/shared/starboard/player/player_internal.h
@@ -60,7 +60,7 @@ struct SbPlayerPrivate {
 
 namespace starboard::shared::starboard::player {
 
-class SbPlayerPrivateImpl final : public SbPlayerPrivate {
+class SbPlayerPrivateImpl : public SbPlayerPrivate {
  public:
   typedef ::starboard::shared::starboard::media::AudioSampleInfo
       AudioSampleInfo;
@@ -75,24 +75,24 @@ class SbPlayerPrivateImpl final : public SbPlayerPrivate {
       void* context,
       std::unique_ptr<PlayerWorker::Handler> player_worker_handler);
 
-  void Seek(int64_t seek_to_time, int ticket) final;
-  void WriteSamples(const SbPlayerSampleInfo* sample_infos,
-                    int number_of_sample_infos) final;
-  void WriteEndOfStream(SbMediaType stream_type) final;
-  void SetBounds(int z_index, int x, int y, int width, int height) final;
-  void GetInfo(SbPlayerInfo* out_player_info) final;
-  void SetPause(bool pause) final;
-  void SetPlaybackRate(double playback_rate) final;
-  void SetVolume(double volume) final;
+  ~SbPlayerPrivateImpl() override;
 
-  SbDecodeTarget GetCurrentDecodeTarget() final;
+  void Seek(int64_t seek_to_time, int ticket) override;
+  void WriteSamples(const SbPlayerSampleInfo* sample_infos,
+                    int number_of_sample_infos) override;
+  void WriteEndOfStream(SbMediaType stream_type) override;
+  void SetBounds(int z_index, int x, int y, int width, int height) override;
+  void GetInfo(SbPlayerInfo* out_player_info) override;
+  void SetPause(bool pause) override;
+  void SetPlaybackRate(double playback_rate) override;
+  void SetVolume(double volume) override;
+
+  SbDecodeTarget GetCurrentDecodeTarget() override;
   bool GetAudioConfiguration(
       int index,
-      SbMediaAudioConfiguration* out_audio_configuration) final;
+      SbMediaAudioConfiguration* out_audio_configuration) override;
 
-  ~SbPlayerPrivateImpl() final;
-
- private:
+ protected:
   SbPlayerPrivateImpl(
       SbMediaAudioCodec audio_codec,
       SbMediaVideoCodec video_codec,
@@ -103,6 +103,9 @@ class SbPlayerPrivateImpl final : public SbPlayerPrivate {
       void* context,
       std::unique_ptr<PlayerWorker::Handler> player_worker_handler);
 
+  std::unique_ptr<PlayerWorker> worker_;
+
+ private:
   void UpdateMediaInfo(int64_t media_time,
                        int dropped_video_frames,
                        int ticket,
@@ -125,8 +128,6 @@ class SbPlayerPrivateImpl final : public SbPlayerPrivate {
   // Used to determine if |worker_| is progressing with playback so that
   // we may extrapolate the media time in GetInfo().
   bool is_progressing_ = false;
-
-  std::unique_ptr<PlayerWorker> worker_;
 
   std::mutex audio_configurations_mutex_;
   std::vector<SbMediaAudioConfiguration> audio_configurations_;

media/starboard/sbplayer_bridge.cc
@@ -38,8 +38,8 @@
 #include "starboard/common/player.h"
 #include "starboard/common/string.h"
 #include "starboard/configuration.h"
+#include "starboard/extension/experimental/video_decoder_configuration.h"
 #include "starboard/extension/player_configurate_seek.h"
-#include "starboard/extension/video_decoder_configuration.h"
 #if COBALT_MEDIA_ENABLE_PLAYER_SET_MAX_VIDEO_INPUT_SIZE
 #include "starboard/extension/player_set_max_video_input_size.h"
 #endif  // COBALT_MEDIA_ENABLE_PLAYER_SET_MAX_VIDEO_INPUT_SIZE
@@ -85,6 +85,12 @@ void SetDiscardPadding(
       discard_padding.first.InMicroseconds();
   sample_info->discarded_duration_from_back =
       discard_padding.second.InMicroseconds();
+}
+
+// Helper to convert std::optional to a C-style pointer for passing through the
+// Starboard extension API boundary.
+const int* ToPtr(const std::optional<int>& value) {
+  return value ? &*value : nullptr;
 }
 
 }  // namespace
@@ -224,11 +230,7 @@ SbPlayerBridge::SbPlayerBridge(
 #endif  // COBALT_MEDIA_ENABLE_DECODE_TARGET_PROVIDER
     const std::string& max_video_capabilities,
     int max_video_input_size,
-    bool flush_decoder_during_reset,
-    bool reset_audio_decoder,
-    std::optional<int> initial_max_frames_in_decoder,
-    std::optional<int> max_pending_input_frames,
-    std::optional<int> video_decoder_poll_interval_ms
+    const ExperimentalFeatures& experimental_features
 #if BUILDFLAG(IS_ANDROID)
     ,
     jobject surface_view
@@ -255,11 +257,7 @@ SbPlayerBridge::SbPlayerBridge(
 #endif  // COBALT_MEDIA_ENABLE_DECODE_TARGET_PROVIDER
       max_video_capabilities_(max_video_capabilities),
       max_video_input_size_(max_video_input_size),
-      flush_decoder_during_reset_(flush_decoder_during_reset),
-      reset_audio_decoder_(reset_audio_decoder),
-      initial_max_frames_in_decoder_(initial_max_frames_in_decoder),
-      max_pending_input_frames_(max_pending_input_frames),
-      video_decoder_poll_interval_ms_(video_decoder_poll_interval_ms)
+      experimental_features_(experimental_features)
 #if BUILDFLAG(IS_ANDROID)
       ,
       surface_view_(surface_view)
@@ -817,9 +815,10 @@ void SbPlayerBridge::CreatePlayer() {
       player_configurate_seek_extension->version >= 1) {
     player_configurate_seek_extension
         ->SetForceFlushDecoderDuringResetForCurrentThread(
-            flush_decoder_during_reset_);
+            experimental_features_.enable_flush_during_seek);
     player_configurate_seek_extension
-        ->SetForceResetAudioDecoderForCurrentThread(reset_audio_decoder_);
+        ->SetForceResetAudioDecoderForCurrentThread(
+            experimental_features_.enable_reset_audio_decoder);
   }
 
   const StarboardExtensionVideoDecoderConfigurationApi*
@@ -831,21 +830,13 @@ void SbPlayerBridge::CreatePlayer() {
       strcmp(video_decoder_configuration_extension->name,
              kStarboardExtensionVideoDecoderConfigurationName) == 0 &&
       video_decoder_configuration_extension->version >= 1) {
-    if (initial_max_frames_in_decoder_) {
-      video_decoder_configuration_extension
-          ->SetVideoInitialMaxFramesInDecoderForCurrentThread(
-              *initial_max_frames_in_decoder_);
-    }
-    if (max_pending_input_frames_) {
-      video_decoder_configuration_extension
-          ->SetVideoMaxPendingInputFramesForCurrentThread(
-              *max_pending_input_frames_);
-    }
-    if (video_decoder_poll_interval_ms_) {
-      video_decoder_configuration_extension
-          ->SetVideoDecoderPollIntervalMsForCurrentThread(
-              *video_decoder_poll_interval_ms_);
-    }
+    StarboardVideoDecoderExperimentalFeatures experimental_features = {
+        ToPtr(experimental_features_.initial_max_frames_in_decoder),
+        ToPtr(experimental_features_.max_pending_input_frames),
+        ToPtr(experimental_features_.video_decoder_poll_interval_ms),
+    };
+    video_decoder_configuration_extension
+        ->SetExperimentalFeaturesForCurrentThread(&experimental_features);
   }
 
 #if BUILDFLAG(IS_ANDROID)

media/starboard/sbplayer_bridge.h
@@ -42,6 +42,7 @@
 #include "media/base/audio_decoder_config.h"
 #include "media/base/decoder_buffer.h"
 #include "media/base/demuxer_stream.h"
+#include "media/base/starboard/starboard_renderer_config.h"
 #include "media/base/video_decoder_config.h"
 #include "media/starboard/sbplayer_interface.h"
 #include "media/starboard/sbplayer_set_bounds_helper.h"
@@ -68,6 +69,8 @@ class SbPlayerBridge {
   // Call to get the SbDecodeTargetGraphicsContextProvider for SbPlayerCreate().
   typedef base::RepeatingCallback<SbDecodeTargetGraphicsContextProvider*()>
       GetDecodeTargetGraphicsContextProviderFunc;
+
+  using ExperimentalFeatures = StarboardRendererConfig::ExperimentalFeatures;
 
 #if SB_HAS(PLAYER_WITH_URL)
   typedef base::Callback<void(const char*, const unsigned char*, unsigned)>
@@ -106,11 +109,7 @@ class SbPlayerBridge {
 #endif  // COBALT_MEDIA_ENABLE_DECODE_TARGET_PROVIDER
                  const std::string& max_video_capabilities,
                  int max_video_input_size,
-                 bool flush_decoder_during_reset,
-                 bool reset_audio_decoder,
-                 std::optional<int> initial_max_frames_in_decoder,
-                 std::optional<int> max_pending_input_frames,
-                 std::optional<int> video_decoder_poll_interval_ms
+                 const ExperimentalFeatures& experimental_features
 #if BUILDFLAG(IS_ANDROID)
                  ,
                  jobject surface_view
@@ -346,11 +345,7 @@ class SbPlayerBridge {
   // Set the maximum size in bytes of an input buffer for video.
   int max_video_input_size_;
 
-  const bool flush_decoder_during_reset_;
-  const bool reset_audio_decoder_;
-  const std::optional<int> initial_max_frames_in_decoder_;
-  const std::optional<int> max_pending_input_frames_;
-  const std::optional<int> video_decoder_poll_interval_ms_;
+  const ExperimentalFeatures experimental_features_;
 
 #if BUILDFLAG(IS_ANDROID)
   // Set the surface to Android Overlay's surface view.

media/starboard/starboard_renderer.cc
@@ -618,11 +618,7 @@ void StarboardRenderer::CreatePlayerBridge() {
         // TODO(b/326825450): Revisit 360 videos.
         kSbPlayerOutputModeInvalid, max_video_capabilities_,
         // TODO(b/326654546): Revisit HTMLVideoElement.setMaxVideoInputSize.
-        -1, experimental_features_.enable_flush_during_seek,
-        experimental_features_.enable_reset_audio_decoder,
-        experimental_features_.initial_max_frames_in_decoder,
-        experimental_features_.max_pending_input_frames,
-        experimental_features_.video_decoder_poll_interval_ms
+        /*max_video_input_size=*/-1, experimental_features_
 #if BUILDFLAG(IS_ANDROID)
         ,
         // TODO: b/475294958 - Revisit platform-specific codes above starboard.

starboard/android/shared/BUILD.gn
@@ -238,6 +238,7 @@ static_library("starboard_platform") {
     "platform_info.cc",
     "platform_info.h",
     "platform_service.cc",
+    "player_android.h",
     "player_components_factory.cc",
     "player_configurate_seek.cc",
     "player_configurate_seek.h",
@@ -295,6 +296,7 @@ static_library("starboard_platform") {
     "video_decoder_configuration.h",
     "video_decoder_configuration_internal.cc",
     "video_decoder_configuration_internal.h",
+    "video_decoder_experimental_features.h",
     "video_frame_tracker.cc",
     "video_frame_tracker.h",
     "video_max_video_input_size.cc",

starboard/android/shared/player_components_factory.cc
@@ -31,7 +31,9 @@
 #include "starboard/android/shared/jni_utils.h"
 #include "starboard/android/shared/media_capabilities_cache.h"
 #include "starboard/android/shared/media_common.h"
+#include "starboard/android/shared/player_android.h"
 #include "starboard/android/shared/video_decoder.h"
+#include "starboard/android/shared/video_decoder_experimental_features.h"
 #include "starboard/common/check_op.h"
 #include "starboard/common/log.h"
 #include "starboard/common/media.h"
@@ -51,6 +53,7 @@
 #include "starboard/shared/starboard/player/filter/video_render_algorithm_impl.h"
 #include "starboard/shared/starboard/player/filter/video_renderer_internal_impl.h"
 #include "starboard/shared/starboard/player/filter/video_renderer_sink.h"
+#include "starboard/shared/starboard/player/player_internal.h"
 
 namespace starboard::android::shared {
 
@@ -576,13 +579,11 @@ class PlayerComponentsFactory : public starboard::shared::starboard::player::
                    << " of " << flush_delay_usec << "us during Flush().";
     }
 
-    VideoDecoder::ExperimentalFeatures experimental_features;
-    experimental_features.initial_max_frames_in_decoder =
-        creation_parameters.video_initial_max_frames_in_decoder();
-    experimental_features.max_pending_input_frames =
-        creation_parameters.video_max_pending_input_frames();
-    experimental_features.video_decoder_poll_interval_ms =
-        creation_parameters.video_decoder_poll_interval_ms();
+    auto* android_player =
+        static_cast<SbPlayerAndroid*>(creation_parameters.player());
+    SB_CHECK(android_player);
+    const VideoDecoder::ExperimentalFeatures& experimental_features =
+        android_player->experimental_features();
     auto video_decoder = std::make_unique<VideoDecoder>(
         creation_parameters.video_stream_info(),
         creation_parameters.drm_system(), creation_parameters.output_mode(),

starboard/android/shared/player_create.cc
@@ -20,6 +20,7 @@
 #include <utility>
 
 #include "starboard/android/shared/configurate_seek.h"
+#include "starboard/android/shared/player_android.h"
 #include "starboard/android/shared/video_decoder_configuration_internal.h"
 #include "starboard/android/shared/video_max_video_input_size.h"
 #include "starboard/android/shared/video_surface_view.h"
@@ -30,11 +31,9 @@
 #include "starboard/configuration.h"
 #include "starboard/decode_target.h"
 #include "starboard/shared/starboard/player/filter/filter_based_player_worker_handler.h"
-#include "starboard/shared/starboard/player/player_internal.h"
 #include "starboard/shared/starboard/player/player_worker.h"
 
 using starboard::shared::starboard::player::PlayerWorker;
-using starboard::shared::starboard::player::SbPlayerPrivateImpl;
 using starboard::shared::starboard::player::filter::
     FilterBasedPlayerWorkerHandler;
 
@@ -215,23 +214,14 @@ SbPlayer SbPlayerCreate(SbWindow window,
           GetForceFlushDecoderDuringResetForCurrentThread());
   handler->SetResetAudioDecoder(
       starboard::android::shared::GetForceResetAudioDecoderForCurrentThread());
-  if (auto initial_max_frames_in_decoder = starboard::android::shared::
-          GetVideoInitialMaxFramesInDecoderForCurrentThread()) {
-    handler->SetVideoInitialMaxFramesInDecoder(*initial_max_frames_in_decoder);
-  }
-  if (auto max_pending_input_frames = starboard::android::shared::
-          GetVideoMaxPendingInputFramesForCurrentThread()) {
-    handler->SetVideoMaxPendingInputFrames(*max_pending_input_frames);
-  }
-  if (auto video_decoder_poll_interval_ms = starboard::android::shared::
-          GetVideoDecoderPollIntervalMsForCurrentThread()) {
-    handler->SetVideoDecoderPollIntervalMs(*video_decoder_poll_interval_ms);
-  }
+
   handler->SetVideoSurfaceView(
       starboard::android::shared::GetSurfaceViewForCurrentThread());
-  SbPlayer player = SbPlayerPrivateImpl::CreateInstance(
+
+  SbPlayer player = starboard::android::shared::SbPlayerAndroid::Create(
       audio_codec, video_codec, sample_deallocate_func, decoder_status_func,
-      player_status_func, player_error_func, context, std::move(handler));
+      player_status_func, player_error_func, context, std::move(handler),
+      starboard::android::shared::GetExperimentalFeaturesForCurrentThread());
 
   if (SbPlayerIsValid(player)) {
     if (creation_param->output_mode != kSbPlayerOutputModeDecodeToTexture) {

commit e90962a0c4c44ad703dde86e908d5474e74b115a

media: Fix use-after-destruction crash in SbPlayerPrivateImpl (#9254)
The background worker thread could attempt to call UpdateMediaInfo and
lock mutex_ during player destruction after mutex_ had already been
destroyed.

By moving worker_ to the end of the class declaration, we ensure it is
destroyed first (joining its thread) before other members like mutex_
are torn down.

Also added is_valid() to SbPlayerPrivateImpl to safely check worker_
initialization state from subclasses like SbPlayerAndroid.

This regression was introduced in #9183 

Issue: 487696177

starboard/android/shared/player_android.h
@@ -43,7 +43,7 @@ class SbPlayerAndroid
         audio_codec, video_codec, sample_deallocate_func, decoder_status_func,
         player_status_func, player_error_func, context,
         std::move(player_worker_handler), experimental_features));
-    if (!player->worker_) {
+    if (!player->is_valid()) {
       return nullptr;
     }
     return player.release();

starboard/shared/starboard/player/player_internal.h
@@ -103,7 +103,7 @@ class SbPlayerPrivateImpl : public SbPlayerPrivate {
       void* context,
       std::unique_ptr<PlayerWorker::Handler> player_worker_handler);
 
-  std::unique_ptr<PlayerWorker> worker_;
+  bool is_valid() const { return worker_ != nullptr; }
 
  private:
   void UpdateMediaInfo(int64_t media_time,
@@ -133,6 +133,9 @@ class SbPlayerPrivateImpl : public SbPlayerPrivate {
   std::vector<SbMediaAudioConfiguration> audio_configurations_;
 
   static int number_of_players_;
+
+  // |worker_| should be placed last, so that it is destroyed first.
+  std::unique_ptr<PlayerWorker> worker_;
 };
 
 }  // namespace starboard::shared::starboard::player