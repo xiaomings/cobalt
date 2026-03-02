a216cc8334f425d8accb15b5fe1259aa76f6666a
Kyujung Youn <kjyoun@google.com>
2026-03-02 13:30:00 -0800

    media: Add Media.VideoDecoderInitialPrerollCount experimental feature (#9298)
    This change introduces an experimental configuration setting to
    customize the initial number of frames the video decoder must buffer
    (preroll) before playback begins on Android. This allows for fine-tuning
    of startup latency and stability.
    
    This re-applies the logic from ce5045e589c using the current plumbing.
    
    Bug: 487097162

--- starboard/android/shared/video_decoder.cc
+++ starboard/android/shared/video_decoder.cc
@@ -409,7 +409,9 @@ VideoDecoder::VideoDecoder(const VideoStreamInfo& video_stream_info,
                                       tunnel_mode_audio_session_id != -1),
       has_new_texture_available_(false),
       surface_condition_variable_(surface_destroy_mutex_),
-      number_of_preroll_frames_(kInitialPrerollFrameCount) {
+      number_of_preroll_frames_(
+          experimental_features.video_decoder_initial_preroll_count.value_or(
+              kInitialPrerollFrameCount)) {
   SB_CHECK(error_message);
 
   if (force_secure_pipeline_under_tunnel_mode) {
@@ -441,12 +443,13 @@ VideoDecoder::VideoDecoder(const VideoStreamInfo& video_stream_info,
     }
   }
 
-  SB_LOG(INFO) << "Created VideoDecoder for codec "
-               << GetMediaVideoCodecName(video_codec_) << ", with output mode "
-               << GetPlayerOutputModeName(output_mode_)
-               << ", max pending input size " << max_pending_inputs_size_
-               << ", max video capabilities \"" << max_video_capabilities_
-               << "\", and tunnel mode audio session id "
+  SB_LOG(INFO) << "Created VideoDecoder for codec="
+               << GetMediaVideoCodecName(video_codec_)
+               << ", with output mode=" << GetPlayerOutputModeName(output_mode_)
+               << ", preroll count=" << number_of_preroll_frames_
+               << ", max pending input size=" << max_pending_inputs_size_
+               << ", max video capabilities=\"" << max_video_capabilities_
+               << "\", and tunnel mode audio session id="
                << tunnel_mode_audio_session_id_;
 }
 
